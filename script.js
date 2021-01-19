4a6108195a391c41f67813dcecf50932931f293d
Sree Ram Mohanan <sreeram17@gmail.com>
2021-01-19 22:15:31 +0000

    update changes

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function compute()
 
         //Displaying info
         document.getElementById("result").innerHTML=
-        'If you deposit '+principal+',<br>at an interest rate of '+rate+'%.<br>You will receive an amount of '+interest+',<br>in the year '+endYear;
+        'If you deposit <mark>'+principal+'</mark>,<br>at an interest rate of <mark>'+rate+'</mark>%.<br>You will receive an amount of <mark>'+interest+'</mark>,<br>in the year <mark>'+endYear+'</mark>';
     }
     else
     {
